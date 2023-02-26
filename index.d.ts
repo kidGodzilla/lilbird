c52eb0da2108e273224e2845ac8044b4dd31ef3e
James Futhey <kidGodzilla@users.noreply.github.com>
2023-02-26 12:49:56 -0800

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,9 @@
 declare namespace Lilbird {}
 
-declare class Lilbird {
+declare interface Lilbird {
     identify<T>(userId: string, userProperties?: T): void;
     track<T>(eventName: string, eventProperties?: T): void;
-    init<T>(properties?: T): void;
+    init<T>(configuration?: T): void;
 }
 
 export = Lilbird;
